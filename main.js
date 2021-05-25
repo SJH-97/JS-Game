0d36efbf4611e8cdfee6b8393f8bd7350653e4f7
Samuel Harrison <samsmacbook@Samuels-MacBook.local>
2021-05-25 15:16:45 +0100

    successfully implemented wordsArr functionality

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const displayTimer = document.querySelector(".countdown-box__clock");
 const inputScreen = document.querySelector(".input-box__text");
 const scoreScreen = document.querySelector(".current-score__text");
 const highScoreScreen = document.querySelector(".high-score-box__text");
+
+// Word Array functionality
+
+// const randomWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+
+// document.getElementById("word").innerHTML = randomWord;
+
+// Countdown Timer functionality
